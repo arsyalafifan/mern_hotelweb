43829a014d631cc32e1efb9cc37fe72fa4837a77
Muh Arsyal Afifan Ersaputra <ersaputra21@gmail.com>
2021-11-20 20:55:11 +0700

    create star element and testimonial section

--- src/elements/Star/index.js
+++ src/elements/Star/index.js
@@ -1,51 +1,76 @@
-import React from 'react'
+import React from "react";
 
-import propTypes from 'prop-types'
+import propTypes from "prop-types";
 
+import "./index.scss";
+export default function Star({ className, value, height, width, spacing }) {
+  const decimals = Number(value) % 1;
 
-export default function Star({className, value, height, width, spacing}) {
+  const star = [];
+  let leftPos = 0;
+  for (let index = 0; index < 5 && index < value - decimals; index++) {
+    leftPos = leftPos + width;
+    console.log(leftPos)
+    star.push(
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing
+        }}
+      ></div>
+    );
+  }
+  if (decimals > 0 && value <= 5)
+    star.push(
+      <div
+        className="star"
+        key={`starWithDecimal`}
+        style={{
+          left: leftPos,
+          height: height,
+          width: decimals * width - spacing
+        }}
+      ></div>
+    );
 
-    const decimals = Number(value) % 1;
-    const star = [];
-    let leftPost = 0;
+  const starPlaceholder = [];
+  for (let index = 0; index < 5; index++) {
+    starPlaceholder.push(
+      <div
+        className="star placeholder"
+        key={`starPlaceholder-${index}`}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing
+        }}
+      ></div>
+    );
+  }
+  console.log(leftPos)
 
-    for (let index = 0; index < 5 && index < value - decimals; index++) {
-        leftPost += width;
-        star.push(
-            <div className="star" key={`star-${index}`} style={{left: index*width, width: width, marginRight: spacing}}></div>
-        )
-        
-    }
-
-    if(decimals > 0 && value <= 5){
-        star.push(
-            <div className="star" key={`star-${index}`} style={{left: leftPost, width: width - spacing}}></div>
-        )
-    }
-
-    const starPlaceholder = []
-    for (let index = 0; index < 5; index++) {
-        starPlaceholder.push(
-            <div className="star placeholder" key={`starPlaceholder-${index}`} style={{left: index*width, width: width, marginRight: spacing}}></div>
-        )
-        
-    }
-
-
-    return (
-        <>
-            <div className={['stars', className.join(" ")]} style={{height: height}}>
-                {starPlaceholder}
-                {star}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className={["stars", className].join(" ")}
+        style={{ height: height }}
+      >
+        {starPlaceholder}
+        {star}
+      </div>
+    </>
+  );
 }
 
 Star.propTypes = {
-    className: propTypes.string,
-    value: propTypes.number,
-    width: propTypes.number,
-    height: propTypes.number,
-    spacing: propTypes.number,
-}
+  className: propTypes.string,
+  value: propTypes.number,
+  width: propTypes.number,
+  height: propTypes.number,
+  spacing: propTypes.number,
+};
